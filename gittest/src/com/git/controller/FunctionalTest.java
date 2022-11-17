79574eceb25b1321aa9cca909eae07a08d1f3b5a
yeonji <yeonji@DESKTOP-OGBT9F7>
2022-11-17 09:35:17 +0900

    main 더하기연산내용 옮김

--- gittest/src/com/git/controller/FunctionalTest.java
+++ gittest/src/com/git/controller/FunctionalTest.java
@@ -4,9 +4,5 @@ public class FunctionalTest {
 	
 	public void printStr() {
 		System.out.println("실행");
-		
-		int a = 10;
-		int b = 20;
-		System.out.println(a+b);
 	}
 }
--- gittest/src/com/git/run/Main.java
+++ gittest/src/com/git/run/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
+		
+		int a = 10;
+		int b = 20;
+		System.out.println(a+b);
 	}
 
 }
