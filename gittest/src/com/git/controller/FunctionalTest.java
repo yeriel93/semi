d55a94c32f9587ffe4c59b1af242455eb4cd0161
yeonji <yeonji@DESKTOP-OGBT9F7>
2022-11-17 09:14:35 +0900

    01_bs 정수변수추가, 더하기출력

--- gittest/src/com/git/controller/FunctionalTest.java
+++ gittest/src/com/git/controller/FunctionalTest.java
@@ -1,7 +1,12 @@
 package com.git.controller;
 
 public class FunctionalTest {
+	
 	public void printStr() {
 		System.out.println("실행");
+		
+		int a = 10;
+		int b = 20;
+		System.out.println(a+b);
 	}
 }
